956d1d5375e2f0db66fa4521d888fa36253fcb40
Zach <zawood11@gmail.com>
2022-02-24 16:44:36 -0500

    completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(int)
+    while int > 1
+        puts "#{int} SECOND(S)!"
+        int -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+    while int > 1
+        puts "#{int} SECOND(S)!"
+        int -= 1
+        sleep(1)
+    end
+    "HAPPY NEW YEAR!"
 end
